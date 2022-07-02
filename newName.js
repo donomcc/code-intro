c7e2ddc3e5ec452478d59ecae382b1432922a39e
Donovan McCollum <donomcc@gmail.com>
2022-07-02 15:29:27 -0500

    refactor: example

--- newName.js
+++ newName.js
@@ -1,1 +1,31 @@
 // this is a comment
+
+const firstName = "Donovan";
+let lastName = "McCollum";
+var age = 25
+
+console.log(firstName, lastName);
+
+let weight = 999;
+
+// console.log(`Weight is: ${weight}`);
+
+let x = 4
+let y = 5
+let z = 6
+
+const sum = x + y;
+// console.log(sum);
+
+const product = y * z;
+// console.log(product);
+
+const newProduct = sum + product;
+// console.log(newProduct);
+
+const string1 = `string 1`
+const string2 = `string 2`
+const string3 = `string 3`
+
+// console.log(string1 + string2);
+
