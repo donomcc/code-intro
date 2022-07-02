d4ef56e860d434e4fc0d85ec78ef03454038d7a9
Donovan McCollum <donomcc@gmail.com>
2022-07-02 15:28:22 -0500

    feature: readline example

---
+++ readLine.js
@@ -0,0 +1,11 @@
+const readLine = require('readline');
+
+const reader = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+reader.question("What is your name? ", function(name) {
+ console.log(`Hi, ${name}`);
+ reader.close();
+})
